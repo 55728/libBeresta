c6ddf04406de2b64a12ac10329c0e4966286a7ac
Dmitry Solomennikov <dmitrys99@mail.ru>
2025-07-28 11:52:40 +0300

    Добавлена генерация кода ECL
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

---
+++ bindings/ecl/ecl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#set -e
+
+SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd -P )
+
+RENDER_DIR=$SCRIPT_DIR/../../gen/
+PREFIX=../bindings/ecl
+
+mkdir $SCRIPT_DIR/res > /dev/null
+
+pushd $RENDER_DIR > /dev/null
+
+./render.sh $PREFIX/ecl-page-sizes.dj page_sizes.lsp ru $PREFIX/res/page_sizes.lisp
+./render.sh $PREFIX/ecl-types.dj types.lsp ru $PREFIX/res/types.lisp
+./render.sh $PREFIX/ecl.dj error.lsp ru $PREFIX/res/error.lisp
+./render.sh $PREFIX/ecl.dj base.lsp ru $PREFIX/res/base.lisp
+
+./render.sh $PREFIX/exp-ecl-page-sizes.dj page_sizes.lsp ru $PREFIX/res/exp-page_sizes.lisp
+./render.sh $PREFIX/exp-ecl-types.dj types.lsp ru $PREFIX/res/exp-types.lisp
+./render.sh $PREFIX/exp-ecl.dj error.lsp ru $PREFIX/res/exp-error.lisp
+./render.sh $PREFIX/exp-ecl.dj base.lsp ru $PREFIX/res/exp-base.lisp
+
+
+popd > /dev/null
