79b9d12545290ab624d90ff90fa426620a4852c8
Dmitry Solomennikov <dmitrys99@mail.ru>
2025-08-16 12:42:02 +0300

    Применены относительные пути
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- bindings/ecl/load.sh
+++ bindings/ecl/load.sh
@@ -3,9 +3,9 @@
 #ecl \
 /home/dmitrys/devel/lisp/ecl-pkg/bin/ecl \
     --eval "(ext:install-c-compiler)" \
-    --eval "(setf c:*user-cc-flags* \"-I/home/dmitrys/devel/libberesta/include -L/home/dmitrys/devel/libberesta/build/src/ -lbrst -fPIC -lz\")" \
-    --eval "(setf c:*user-linker-libs* \"-L/home/dmitrys/devel/libberesta/build/src/ -lbrst -fPIC -lz\")" \
-    --eval "(pushnew  #P\"/home/dmitrys/devel/libberesta/bindings/ecl/\" asdf:*central-registry*)" \
+    --eval "(setf c:*user-cc-flags* \"-I../../include -L../../build/src/ -lbrst -fPIC -lz\")" \
+    --eval "(setf c:*user-linker-libs* \"-L../../build/src/ -lbrst -fPIC -lz\")" \
+    --eval "(pushnew  #P\"$(realpath .)/\" asdf:*central-registry*)" \
     --eval "(asdf:load-system 'cl-beresta :verbose t :force t)"
 
 	#--eval "(princ (format nil \"~A\" compiler::*default-machine*))" \
