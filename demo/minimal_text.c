1093aca3ed95e35aef879c3b6fd3d2d825feec79
Dmitry Solomennikov <dmitrys99@mail.ru>
2025-09-20 20:09:58 +0300

    Реализация текста внутри XObject (пока без включения шрифта)
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

---
+++ demo/minimal_text.c
@@ -0,0 +1,85 @@
+/*
+ * `libBeresta`
+ *
+ * minimal.c - Минимально возможный пример
+ * =========
+ *
+ * Copyright (c) 2025 Dmitry Solomennikov
+ *
+ * Текст лицензии смотри в файле `ЛИЦЕНЗИЯ`
+ */
+
+/** en
+  \par Minimal text BRST program demo
+
+*/
+
+/** ru
+  \par Минимальная программа библиотеки libBeresta для вывода текста.
+
+  Минимально возможная программа для создания PDF-документа.
+  Создается \ref BRST_Doc, в него добавляется объект \ref BRST_Page,
+  устанавливаются размер и ориентация страницы и документ сохраняется.
+
+  Последним действием все очищается.
+*/
+
+#include "brst.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    BRST_Doc pdf;
+    BRST_Page page;
+    char fname[256];
+
+    strcpy(fname, argv[0]);
+    strcat(fname, ".pdf");
+
+    // Создание объекта документа
+    pdf = BRST_Doc_New_Empty();
+    if (!pdf) {
+        printf("Error: cannot create Doc object\n");
+        return 1;
+    }
+
+    // Добавление страницы
+    page = BRST_Doc_Page_Add(pdf);
+
+    // Настройка размера и ориентации страницы
+    BRST_Page_SetSize(page, BRST_PAGE_SIZE_A4, BRST_PAGE_ORIENTATION_LANDSCAPE);
+
+#if 1
+    BRST_Doc_UseUTFEncodings(pdf);
+    BRST_Doc_Encoder_SetCurrent(pdf, "UTF-8");
+
+    // Последний параметр отвечает за встраивание шрифта внутрь файла.
+    BRST_CSTR font_name = BRST_Doc_TTFont_LoadFromFile(pdf, "../../data/fonts/PT/PT/PT-Astra-Serif/pt-astra-serif_regular.ttf", BRST_TRUE);
+    BRST_Font font = BRST_Doc_Font(pdf, font_name, "UTF-8");
+#else
+    BRST_Font font = BRST_Doc_Font(pdf, "Helvetica", NULL);
+#end
+
+    // Вход в текстовый режим
+    BRST_Page_BeginText(page);
+
+    // Установка выбранного шрифта и размера для текста
+    BRST_Page_SetFontAndSize(page, font, 20);
+
+    // Отображение одной буквы
+    BRST_Page_MoveTextPos(page, 0, 0);
+    BRST_Page_ShowText(page, "W");
+
+    // Выход из текстового режима
+    BRST_Page_EndText(page);
+
+
+    // Сохранение документа в файл
+    BRST_Doc_SaveToFile(pdf, fname);
+
+    // Очистка
+    BRST_Doc_Free(pdf);
+
+    return 0;
+}
---
+++ demo/xobject_text.c
@@ -0,0 +1,105 @@
+/*
+ * `libBeresta`
+ *
+ * xobject_text.c - Создание и отображение XObject Form
+ * ==============
+ *
+ * Copyright (c) 2025 Dmitry Solomennikov
+ *
+ * Текст лиценции смотри в файле `ЛИЦЕНЗИЯ`
+ */
+
+/** en
+  \par XObject Form Text usage
+
+
+*/
+
+/** ru
+  \par Использование текста в XObject Form
+*/
+
+#include "brst.h"
+#include "handler.h"
+#include <setjmp.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    BRST_Doc pdf;
+    BRST_Page page;
+    char fname[256];
+
+    strcpy(fname, argv[0]);
+    strcat(fname, ".pdf");
+
+    // Создание объекта документа
+    pdf = BRST_Doc_New(demo_error_handler, NULL);
+    if (!pdf) {
+        printf("Error: cannot create Doc object\n");
+        return 1;
+    }
+
+    if (setjmp(env)) {
+        BRST_Doc_Free(pdf);
+        return 1;
+    }
+
+#if 1
+    BRST_Doc_UseUTFEncodings(pdf);
+    BRST_Doc_Encoder_SetCurrent(pdf, "UTF-8");
+    BRST_CSTR font_name = BRST_Doc_TTFont_LoadFromFile(pdf, "../../data/fonts/PT/PT/PT-Astra-Serif/pt-astra-serif_regular.ttf", BRST_FALSE);
+    BRST_Font font = BRST_Doc_Font(pdf, font_name, "UTF-8");
+#else
+    BRST_Font font = BRST_Doc_Font(pdf, "Helvetica", NULL);
+#endif
+
+    // Добавление страницы
+    page = BRST_Doc_Page_Add(pdf);
+
+    // Настройка размера и ориентации страницы
+    BRST_Page_SetSize(page, BRST_PAGE_SIZE_A4, BRST_PAGE_ORIENTATION_LANDSCAPE);
+
+    // Подготовка положения объектов
+    BRST_REAL width = BRST_Page_Width(page);
+    BRST_REAL height = BRST_Page_Height(page);
+
+    // Создание XObject Form
+    BRST_XObject xobj = BRST_Doc_Page_XObject_Create(pdf, page, 100, 100, 1, 1);
+    BRST_Dict_SetFontAndSize(xobj, font, 24);
+
+    // Получение и наполнение потока XObject
+    BRST_Stream stream = BRST_XObject_Stream(xobj);
+    BRST_Stream_Rectangle(stream, 0, 0, 100, 100);
+    //BRST_Stream_MoveTo(stream, 0, 0);
+    //BRST_Stream_LineTo(stream, 100, 100);
+    BRST_Stream_Stroke(stream);
+
+    BRST_Stream_BeginText(stream);
+                                           
+    BRST_Stream_MoveTextPos(stream, 0, 0);
+    BRST_Stream_ShowText(stream, font, "Ю");
+
+    BRST_Stream_EndText(stream);
+
+    // Позиционирование и отображение XObject
+    BRST_Page_Translate(page, width / 2 - 50, height / 2 - 50);
+    BRST_Page_XObject_Execute(page, xobj);
+
+    // Позиционирование и отображение XObject на новой странице
+    page = BRST_Doc_Page_Add(pdf);
+    BRST_Page_SetSize(page, BRST_PAGE_SIZE_A4, BRST_PAGE_ORIENTATION_LANDSCAPE);
+    BRST_Page_Translate(page, width / 2, height / 2);
+    BRST_Page_RotateDeg(page, -45);
+    BRST_Page_Translate(page, -50, -50);
+    BRST_Page_XObject_Execute(page, xobj);
+
+    // Сохранение документа в файл
+    BRST_Doc_SaveToFile(pdf, fname);
+
+    // Очистка
+    BRST_Doc_Free(pdf);
+
+    return 0;
+}
