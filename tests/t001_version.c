ec79cb4c9b0ded49c79d73829b4fee57986eb858
Dmitry Solomennikov <dmitrys99@mail.ru>
2025-10-12 23:13:47 +0300

    Добавлено начало тестового фреймворка
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

---
+++ tests/t001_version.c
@@ -0,0 +1,9 @@
+#include "brst.h"
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("BRST TEST:t001: Library version\n");
+
+    printf("%s\n", BRST_Version());
+    return 0;
+}
---
+++ tests/t002_minimal_empty.c
@@ -0,0 +1,36 @@
+#include "brst.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    printf("BRST TEST:t002: Create minimal document.\n");
+    
+    BRST_Doc pdf;
+    BRST_Page page;
+    char fname[256];
+
+    strcpy(fname, argv[0]);
+    strcat(fname, ".pdf");
+
+    // Создание объекта документа
+    pdf = BRST_Doc_New_Empty();
+    if (!pdf) {
+        printf("Error: cannot create Doc object\n");
+        return 1;
+    }
+
+    // Добавление страницы
+    page = BRST_Doc_Page_Add(pdf);
+
+    // Настройка размера и ориентации страницы
+    BRST_Page_SetSize(page, BRST_PAGE_SIZE_A4, BRST_PAGE_ORIENTATION_LANDSCAPE);
+
+    // Сохранение документа в файл
+    BRST_Doc_SaveToFile(pdf, fname);
+
+    // Очистка
+    BRST_Doc_Free(pdf);
+
+    return 0;
+}
