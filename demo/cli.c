a022ddaf465a8b2c70a8dc8b1c7ebafb4ff868e7
Dmitry Solomennikov <dmitrys99@mail.ru>
2026-04-20 22:52:52 +0300

    Fix CWE-134
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- demo/cli.c
+++ demo/cli.c
@@ -7,14 +7,25 @@ char _fname[FNAME_BUFFER_SIZE];
 
 char* prepare_output(int argc, char** argv) {
   if (argc > 0 && argv[0] != NULL) {
-      int written = snprintf(_fname, FNAME_BUFFER_SIZE, "%s.pdf", argv[0]);
-      if (written < 0 || written >= FNAME_BUFFER_SIZE) {
-          printf("Error: program name too long for buffer\n");
+      size_t src_len = strlen(argv[0]);
+
+      // Проверка, что имя программы не слишком длинное
+      if (src_len > FNAME_BUFFER_SIZE - 5) { // -5 для ".pdf" и нулевого байта
+          printf("Error: program name too long\n");
           return NULL;
       }
+
+      // Безопасное копирование имени программы
+      strncpy(_fname, argv[0], FNAME_BUFFER_SIZE - 1);
+      _fname[FNAME_BUFFER_SIZE - 1] = '\0';
+
+      // Безопасное добавление расширения
+      strncat(_fname, ".pdf", FNAME_BUFFER_SIZE - strlen(_fname) - 1);
   } else {
+      // Имя по умолчанию, если argv[0] недоступен
       strncpy(_fname, "output.pdf", FNAME_BUFFER_SIZE - 1);
       _fname[FNAME_BUFFER_SIZE - 1] = '\0';
   }
+
   return (char*)&_fname;
 }
